dc228ed110ae5173af75ac8847468c93f876bde5
DragonController <gracefulfowler@gmail.com>
2022-03-17 19:50:00 -0700

    added test for the values of a tile set

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -16,7 +16,16 @@ describe 'A tile set' do
     end
 
     it 'returns an array of tiles whose values increase from 1, by 1' do
-      skip
+      tile_set = TileSet.generate
+      expect(tile_set[0].value).to be(1)
+      expect(tile_set[1].value).to be(2)
+      expect(tile_set[2].value).to be(3)
+      expect(tile_set[3].value).to be(4)
+      expect(tile_set[4].value).to be(5)
+      expect(tile_set[5].value).to be(6)
+      expect(tile_set[6].value).to be(7)
+      expect(tile_set[7].value).to be(8)
+      expect(tile_set[8].value).to be(9)
     end
 
   end
